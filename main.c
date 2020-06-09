0c569a3432cb20bb8e43198ab712f168a42045a4
sadawi <hahli144@hotmail.com>
2020-06-09 15:55:13 +0300

    remove debug prints, clean up code

--- main.c
+++ main.c
@@ -6,30 +6,15 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 18:44:39 by sadawi            #+#    #+#             */
-/*   Updated: 2020/06/09 15:00:38 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/06/09 15:50:49 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/filler.h"
-#include <stdio.h>///
-#include <fcntl.h>///
-
-
-void	game_ended()
-{
-	while (1)
-		ft_printf("0 0\n");
-}
 
 int		handle_error(char *message)
 {
-	//ft_fprintf(2, "Error: %s", message);
-	//open only for debug
-	int fd = open(DEBUG_OUTPUT, O_WRONLY);///
-	ft_fprintf(fd, "%s\n", message);///
 	ft_fprintf(2, "%s\n", message);
-	close(fd);///
-	//free memory
 	return (-1);
 }
 
@@ -38,11 +23,11 @@ int		get_player_id(t_filler *filler)
 	char *line;
 
 	if (get_next_line(0, &line) != 1)
-		return handle_error(ERROR_READ_PLAYER_ID);
+		return (handle_error(ERROR_READ_PLAYER_ID));
 	if (!ft_strnequ(VM_PLAYER1, line, 11) && !ft_strnequ(VM_PLAYER2, line, 11))
 	{
 		free(line);
-		return handle_error(ERROR_INVALID_PLAYER_ID);
+		return (handle_error(ERROR_INVALID_PLAYER_ID));
 	}
 	if (ft_atoi(ft_strchr(line, 'p') + 1) == 1)
 	{
@@ -58,7 +43,7 @@ int		get_player_id(t_filler *filler)
 	return (0);
 }
 
-t_filler	*init_filler()
+t_filler	*init_filler(void)
 {
 	t_filler *filler;
 
@@ -71,7 +56,7 @@ void	free_2d_array(char **arr)
 	int i;
 
 	i = 0;
-	while(arr[i])
+	while (arr[i])
 		free(arr[i++]);
 	free(arr);
 }
@@ -95,19 +80,17 @@ int		check_map_info(char **map_info)
 int		init_map(t_filler *filler)
 {
 	size_t	i;
-	char 	*line;
-	char 	**map_info;
+	char	*line;
+	char	**map_info;
 
 	if (get_next_line(0, &line) != 1)
 		return (handle_error(ERROR_INVALID_MAP));
-	//debug_print_line("*************8");//
-	//debug_print_line(line);//
 	map_info = ft_strsplit(line, ' ');
 	free(line);
 	if (check_map_info(map_info) == -1)
 	{
 		free_2d_array(map_info);
-		return handle_error(ERROR_INVALID_MAP);
+		return (handle_error(ERROR_INVALID_MAP));
 	}
 	filler->map_height = ft_atoi(map_info[1]);
 	filler->map_width = ft_atoi(map_info[2]);
@@ -117,7 +100,7 @@ int		init_map(t_filler *filler)
 	filler->map = (char**)ft_memalloc(sizeof(char *) * filler->map_height);
 	i = 0;
 	while (i < filler->map_height)
-		filler->map[i++] = (char*)ft_memalloc(sizeof(char) * filler->map_width + 1);
+		filler->map[i++] = (char*)ft_memalloc(filler->map_width + 1);
 	return (0);
 }
 
@@ -126,7 +109,7 @@ int		skip_coordinates(void)
 	char *line;
 
 	if (get_next_line(0, &line) != 1)
-		return handle_error(ERROR_INVALID_MAP);
+		return (handle_error(ERROR_INVALID_MAP));
 	free(line);
 	return (0);
 }
@@ -176,11 +159,8 @@ int		get_map(t_filler *filler)
 	i = 0;
 	while (i < filler->map_height)
 	{
-		debug_print_line("test");
 		if (get_next_line(0, &line) != 1)
 			return (handle_error(ERROR_INVALID_MAP));
-		debug_print_line("wow");
-		debug_print_line(line);
 		if (!check_map_line_valid(filler, line))
 		{
 			free(line);
@@ -222,12 +202,13 @@ int		init_piece(t_filler *filler)
 	if (check_piece_info(piece_size) == -1)
 	{
 		free_2d_array(piece_size);
-		return handle_error(ERROR_INVALID_PIECE);
+		return (handle_error(ERROR_INVALID_PIECE));
 	}
 	filler->piece.height = ft_atoi(piece_size[1]);
 	filler->piece.width = ft_atoi(piece_size[2]);
 	free_2d_array(piece_size);
-	filler->piece.token = (char**)ft_memalloc(sizeof(char *) * filler->piece.height);
+	filler->piece.token = (char**)ft_memalloc(sizeof(char*)
+		* filler->piece.height);
 	return (0);
 }
 
@@ -254,7 +235,6 @@ int		get_piece(t_filler *filler)
 		if (get_next_line(0, &filler->piece.token[i++]) != 1)
 			return (handle_error(ERROR_INVALID_PIECE));
 	}
-	//debug_print_piece(filler);//
 	if (!check_piece_valid(filler))
 		return (handle_error(ERROR_INVALID_PIECE));
 	return (0);
@@ -279,7 +259,7 @@ void	get_y_offset(t_filler *filler)
 		if (!ft_strchr(filler->piece.token[i++], '*'))
 			filler->piece.offset_y++;
 		else
-			break;
+			break ;
 	}
 }
 
@@ -317,7 +297,7 @@ void	get_piece_width(t_filler *filler)
 			if (filler->piece.token[row][col] == '*')
 			{
 				filler->piece.piece_width++;
-				break;
+				break ;
 			}
 			row++;
 		}
@@ -344,9 +324,6 @@ void	get_piece_dimensions(t_filler *filler)
 	get_x_offset(filler);
 	get_piece_width(filler);
 	get_piece_height(filler);
-	
-	//debug_print_line(ft_sprintf("y: %d, x: %d", filler->piece.offset_y, filler->piece.offset_x));
-	//debug_print_line(ft_sprintf("SIZE: h: %d, w: %d", filler->piece.piece_height, filler->piece.piece_width));
 }
 
 int		check_move_valid(t_filler *filler, int y, int x)
@@ -362,13 +339,10 @@ int		check_move_valid(t_filler *filler, int y, int x)
 		col = 0;
 		while (col < filler->piece.width && overlap_count < 2)
 		{
-			//debug_print_line(ft_sprintf("COORDINATES: %d %d %d\n", col, row, overlap_count));
 			if (filler->piece.token[row][col] == '*')
 			{
 				if (filler->map[y + row][x + col] == filler->opponent_symbol)
-				{
 					return (0);
-				}
 				if (filler->map[y + row][x + col] == filler->player_symbol)
 					overlap_count++;
 			}
@@ -385,17 +359,17 @@ int		find_first_valid(t_filler *filler, int *y_coord, int *x_coord)
 {
 	size_t	col;
 	size_t	row;
-	
-	row = 0; //could change this and col to start from [piece height + width] offset area from our squares
+
+	row = 0;
 	while (row < filler->map_height - filler->piece.piece_height + 1)
 	{
 		col = 0;
 		while (col < filler->map_width - filler->piece.piece_width + 1)
 		{
-			//debug_print_line(ft_sprintf("Checking: %d %d\n", row, col));
-			if (check_move_valid(filler, row - filler->piece.offset_y, col - filler->piece.offset_x))
+			if (check_move_valid(filler, row - filler->piece.offset_y,
+				col - filler->piece.offset_x))
 			{
-				*y_coord = row -filler->piece.offset_y;
+				*y_coord = row - filler->piece.offset_y;
 				*x_coord = col - filler->piece.offset_x;
 				return (1);
 			}
@@ -417,12 +391,29 @@ t_moves	*new_move(int x, int y)
 	return (move);
 }
 
+t_moves	*add_valid_move(t_filler *filler, int col, int row, t_moves *tmp)
+{
+	if (!tmp)
+	{
+		tmp = new_move(col - filler->piece.offset_x,
+		row - filler->piece.offset_y);
+		filler->valid_moves = tmp;
+	}
+	else
+	{
+		tmp->next = new_move(col - filler->piece.offset_x,
+		row - filler->piece.offset_y);
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
+
 void	find_valid_moves(t_filler *filler)
 {
 	size_t	col;
 	size_t	row;
 	t_moves *tmp;
-	
+
 	tmp = NULL;
 	row = 0;
 	while (row < filler->map_height - filler->piece.piece_height + 1)
@@ -430,53 +421,19 @@ void	find_valid_moves(t_filler *filler)
 		col = 0;
 		while (col < filler->map_width - filler->piece.piece_width + 1)
 		{
-			if (check_move_valid(filler, row - filler->piece.offset_y, col - filler->piece.offset_x))
-			{
-				if (!tmp)
-				{
-					tmp = new_move(col - filler->piece.offset_x,
-					row -filler->piece.offset_y);
-					filler->valid_moves = tmp;
-				}
-				else
-				{
-					tmp->next = new_move(col - filler->piece.offset_x,
-					row -filler->piece.offset_y);
-					tmp = tmp->next;
-				}
-			}
+			if (check_move_valid(filler, row - filler->piece.offset_y,
+				col - filler->piece.offset_x))
+				tmp = add_valid_move(filler, col, row, tmp);
 			col++;
 		}
 		row++;
 	}
 }
 
-#include <stdlib.h>// for random()
-#include <time.h>//
-int		random_algorithm(t_filler *filler, int *y, int *x)
-{
-	t_moves *tmp;
-	int		move_amt = 0;
-	time_t 	t;
-
-	if (!filler->valid_moves)
-		return (-1);
-	srand((unsigned) time(&t));
-	for (t_moves *tmp = filler->valid_moves; tmp; tmp = tmp->next)
-		move_amt++;
-	int random_idx = rand() % move_amt;
-	tmp = filler->valid_moves;
-	while (random_idx--)
-		tmp = tmp->next;
-	*y = tmp->y;
-	*x = tmp->x;
-	return (0);
-}
-
 void	free_moves(t_filler *filler)
 {
 	t_moves *tmp;
-	
+
 	while (filler->valid_moves)
 	{
 		tmp = filler->valid_moves->next;
@@ -489,7 +446,7 @@ int		**init_heatmap(t_filler *filler)
 {
 	size_t	i;
 	size_t	j;
-	int 	**heatmap;
+	int		**heatmap;
 
 	heatmap = (int**)ft_memalloc(sizeof(int*) * filler->map_height);
 	i = 0;
@@ -525,7 +482,8 @@ void	heatmap(t_filler *filler, int x, int y)
 		col = 0;
 		while (col < filler->map_width)
 		{
-			value = ft_abs(col - x) > ft_abs(row - y) ? ft_abs(col - x) : ft_abs(row - y);
+			value = ft_abs(col - x) > ft_abs(row - y)
+				? ft_abs(col - x) : ft_abs(row - y);
 			if (filler->heatmap[row][col] > value)
 				filler->heatmap[row][col] = value;
 			col++;
@@ -554,19 +512,6 @@ void	make_map_heatmap(t_filler *filler)
 	}
 }
 
-void	print_heatmap(t_filler *filler)
-{
-	size_t	row;
-
-	row = 0;
-	while (row < filler->map_height)
-	{
-		debug_print_line_heatmap(filler->heatmap[row], filler->map_width);
-		row++;
-	}
-	debug_print_line("\n");
-}
-
 void	free_heatmap(t_filler *filler)
 {
 	size_t i;
@@ -590,7 +535,6 @@ void	get_move_score(t_filler *filler, t_moves *move)
 		while (col < filler->piece.width)
 		{
 			if (filler->piece.token[row][col] == '*')
-				
 				move->score += filler->heatmap[move->y + row][move->x + col];
 			col++;
 		}
@@ -615,7 +559,7 @@ void	heatmap_algorithm(t_filler *filler, int *y, int *x)
 {
 	t_moves *best_move;
 	t_moves *tmp;
-	
+
 	tmp = filler->valid_moves;
 	best_move = tmp;
 	while (tmp)
@@ -633,10 +577,8 @@ int		place_piece(t_filler *filler)
 	int		y;
 	int		x;
 
-	
 	get_piece_dimensions(filler);
 	make_map_heatmap(filler);
-	//print_heatmap(filler);
 	find_valid_moves(filler);
 	if (!filler->valid_moves)
 	{
@@ -646,15 +588,9 @@ int		place_piece(t_filler *filler)
 		return (-1);
 	}
 	score_moves(filler);
-	//for (t_moves *tmp = filler->valid_moves; tmp; tmp = tmp->next)
-		//debug_print_line(ft_sprintf("VALID MOVE: %d %d", tmp->x, tmp->y));
-	//if (find_first_valid(filler, &y, &x) == -1)
-		//return (-1);
 	heatmap_algorithm(filler, &y, &x);
-	//random_algorithm(filler, &y, &x);
 	ft_printf("%d %d\n", y, x);
-	//debug_print_line(ft_sprintf("COORDINATES: %d %d\n", y, x));
-	free_moves(filler); //function to free allocated list of moves
+	free_moves(filler);
 	free_heatmap(filler);
 	return (0);
 }
@@ -694,17 +630,14 @@ int		main(void)
 	while (1)
 	{
 		if (parse_input(filler) == -1)
-			break;
+			break ;
 		if (place_piece(filler) == -1)
 		{
 			free_piece(filler);
-			break;
+			break ;
 		}
-		debug_print_map(filler);
 		free_piece(filler);
 	}
-	//FREE MeMORY!!!!!
-	free_memory(filler); //free map and struct
-	//game_ended(); //Program should end in someway if executed without VM
+	free_memory(filler);
 	return (0);
 }
